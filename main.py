d28005dd7479c839212a2bbc5a9450b5a1164815
LauGds <137927575+LauGds@users.noreply.github.com>
2023-07-04 11:54:45 -0700

    main.py

--- main.py
+++ main.py
@@ -23,9 +23,6 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
-#Write your code below this line 👇
 one = input('You are walking throught the jungle and you find two paths which one you chose "right" or "left"? \n')
 one_lower = one.lower()
 if one_lower == "left":
@@ -46,5 +43,3 @@ if one_lower == "left":
     print("Rghhh!, You were attack by trout. Game Over.")
 else:
   print("Ups! You fall into a hole. Game Over.")
-
-
